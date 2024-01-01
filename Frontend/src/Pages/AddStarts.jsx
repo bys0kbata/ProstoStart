a6072a3b37c94ea65a810a815ea0e39d5a6067f9
Bys0kbata <92207641+bys0kbata@users.noreply.github.com>
2024-01-01 20:16:15 +0300

    рп

--- Frontend/src/Pages/AddStarts.jsx
+++ Frontend/src/Pages/AddStarts.jsx
@@ -1,7 +1,29 @@
+import {useState} from "react"
+import "./css/AddStarts.css"
+
 export default function AddStart(){
+ const [nameStart,setNS] = useState("");
+ const [catStart,setCS] = useState("");
+ const [AboutStart,setAS] = useState("");
+ const data = {
+    nameStart: nameStart,
+    catStart: catStart,
+    AboutStart: AboutStart,
+ }
+
+    const onPost = (e)=>{
+        e.prefaulDefault();
+        console.log(data);
+    }
     return(
         <div>
             <h1>Добавление Старта</h1>
+            <form>
+                <input type="text" placeholder="Введите название стартапа" onChange={(e)=>{setNS(e.target.value)}}></input>
+                <input type="text" placeholder="Введите тематику стартапа" onChange={(e)=>{setCS(e.target.value)}}></input>
+                <input type="text" placeholder="Расскажите о сути стартапа"onChange={(e)=>{setAS(e.target.value)}}></input>
+                <button onClick={()=>{onPost(e)}}>Создать</button>
+            </form>
         </div>
     )
 }
